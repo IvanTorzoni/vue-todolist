75ecd5ff243bae5ffd05e331c136106ec41c7609
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-04-19 23:49:47 +0200

    feat: addad enter key function to input

--- js/script.js
+++ js/script.js
@@ -1,38 +1,44 @@
 const { createApp } = Vue
 
 createApp({
-  data() {
-    return {
-        newToDo: 
+    data() {
+        return {
+            newToDo:
             {
                 text: "",
                 done: false
-            }, 
-        done: 'done',
-        todoList: [
-            {
-                text: "Recruiting blog post",
-                done: true
             },
-            {
-                text: "Mobile app luaunch",
-                done: true
-            },
-            {
-                text: "Interview John H.",
-                done: false
-            },
-            {
-                text: "Summit update to mobile storefronts",
-                done: true
-            },
-        ]
-    }
-  },
-  methods: {
-    addToDo: function () {
-        const copyToDo = { ...this.newToDo };
-        this.todoList.push(copyToDo);
-        this.newToDo.text = ""
-  }}
+            done: 'done',
+            todoList: [
+                {
+                    text: "Recruiting blog post",
+                    done: true
+                },
+                {
+                    text: "Mobile app luaunch",
+                    done: true
+                },
+                {
+                    text: "Interview John H.",
+                    done: false
+                },
+                {
+                    text: "Summit update to mobile storefronts",
+                    done: true
+                },
+            ]
+        }
+    },
+    methods: {
+        addToDo: function () {
+            const copyToDo = { ...this.newToDo };
+            this.todoList.push(copyToDo);
+            this.newToDo.text = ""
+        },
+        submit: function () {
+            const copyToDo = { ...this.newToDo };
+            this.todoList.push(copyToDo);
+            this.newToDo.text = ""
+        },
+    },
 }).mount('#app')
